fd4d419fc27eda212807cb0d8ea51d762f8f61a5
JuanDiPosada <juandiegocifuentesposada@gmail.com>
2025-04-25 19:58:10 -0500

    creacion de migraciones

---
+++ database/migrations/2025_04_26_003004_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('course number');
+            $table->integer('day');
+            $table->unsignedBigInteger('area_id')->nullable();
+            $table->foreign('area_id')
+                ->references('id')
+                ->on('areas')
+                ->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
